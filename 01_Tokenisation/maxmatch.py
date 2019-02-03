3849a9ab2763d50a8ac9865cb5e812621968d366
Samuel Maginot <samagino@indiana.edu>
2019-02-03 14:42:35 -0500

    updating so I can clone elsewhere

--- 01_Tokenisation/maxmatch.py
+++ 01_Tokenisation/maxmatch.py
@@ -11,57 +11,65 @@ make stuff iterative, not recursive (maybe, but I guess sentences don't usually 
 
 import sys
 
-
 def makeDictionary(filePath):
-    SAMPLE_SIZE = 1024
     dictionary = set()
 
     # open dictionary file
     with open(filePath, "r") as f:
 
-        sample = f.read(SAMPLE_SIZE)
-        while sample:
+        sample = f.read()
 
-            # split sample into lines
-            sample = sample.split('\n')
+        # split sample into lines
+        sample = sample.split('\n')
+        
+        #sys.stderr.write(str(sample) + '\n')
+        for line in sample:
+            fields = line.split('\t')
+                
+            # if line is empty, continue
+            if len(fields) < 2:
+                continue
+            
             # word entries begin with a number in CoNLLu format
-            if sample[0].isdigit() and len(sample[1]) > 1 and not sample[1].isdigit():
+            if fields[0].isdigit() and (len(fields[1]) > 1) and (not fields[1].isdigit()):
                 # the second collumn in word entries contain the word literal
-                dictionary.add(sample[1])
+                dictionary.add(fields[1])
 
-            sample = f.read(SAMPLE_SIZE)
+    return dictionary
 
-
-
+# [string, set] -> string
+# take tokenized sentence and dictionary,
+# return tokenized words in sentence split by newlines
 def maxMatch(sentence, dictionary):
-    if len(sentence) == 0:
-        return "" # or some other empty list
+
+    # base case for recursion
+    if len(sentence) < 2:
+        return "" # empty list of type string
 
     # reversed, so start at end of sentence and work back
     for i in reversed(range(2, len(sentence) - 1)):
         # first i chars in sentence
         firstword = sentence[0 : i]
+
         # rest of chars in sentence
-        remainder = sentence[i+1 : len(sentence)]
+        remainder = sentence[i : len(sentence)]
 
         # TODO somehow make this not recursive
-        if firstword in dictionary or firstword.isdigit():
+        if (firstword in dictionary) or (firstword.isdigit()):
             return firstword + '\n' + maxMatch(remainder, dictionary)
 
-    # also this one
-    return firstword + maxMatch(remainder, dictionary)
+        # also this one
+        if i == 2:
+            return firstword + maxMatch(remainder, dictionary)
 
 
-dictionary = makeDictionary(argv[1])
-# dunno if I should align samples with sentece boundaries
-SAMPLE_SIZE = 1024
+dictionary = makeDictionary(sys.argv[1])
 
-sample = sys.stdin.read(SAMPLE_SIZE)
-while sample:
-    # assume input has been segmented
-    sentences = sample.split('\n')
+sample = sys.stdin.read()
+# assume input has been segmented
+sentences = sample.split('\n')
 
-    for sentence in sentences:
-        sys.stdout.write(maxMatch(sentence, dictionary) + '\n')
-
-    sample = stdin.read(SAMPLE_SIZE)
+for sentence in sentences:
+    tokens = maxMatch(sentence, dictionary)
+    sys.stdout.write(tokens + '\n')
+    
--- 01_Tokenisation/punkt_segmenter.py
+++ 01_Tokenisation/punkt_segmenter.py
@@ -1,12 +1,14 @@
 import sys
+import nltk.data
 from nltk.tokenize import sent_tokenize
 
+tokenizer = nltk.data.load('tokenizers/punkt/japanese.pickle')
 SAMPLE_SIZE = 1024
 
 sample = sys.stdin.read(SAMPLE_SIZE)
 while sample:
 
-    tokenized_sents = sent_tokenize(sample)
+    tokenized_sents = tokenizer.tokenize(sample)
 
     for sentence in tokenized_sents:
         sys.stdout.write(sentence + '\n')
