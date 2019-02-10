c6b46d8f7e36e1c2477c9ad706d1f5466f09df18
Samuel Maginot <samagino@indiana.edu>
2019-02-10 15:30:31 -0500

    I think maxmatch.py works now

--- 01_Tokenisation/maxmatch.py
+++ 01_Tokenisation/maxmatch.py
@@ -5,13 +5,14 @@ it's prolly not, the training data file is 9 MiB, so that's not that big
 
 
 TODO:
-thing that unpacks dictionary file
 make stuff iterative, not recursive (maybe, but I guess sentences don't usually get that big)
 """
 
 import sys
 
 def makeDictionary(filePath):
+    # want a set because they have constant lookup time complexity in python
+    #    (hash table thing)
     dictionary = set()
 
     # open dictionary file
@@ -30,7 +31,13 @@ def makeDictionary(filePath):
             if len(fields) < 2:
                 continue
             
-            # word entries begin with a number in CoNLLu format
+            # word entries begin with a number in CoNLLu format (first condition)
+            # don't want to populate dictionary with single character words
+            #    because in maxmatch we assume that if we get down to a single
+            #    character, then it's a word (otherwise waste memory) (second
+            #    condition)
+            # don't want to populate dictionary with numbers because in maxmatch
+            #    I assume all numbers are words (third condition)
             if fields[0].isdigit() and (len(fields[1]) > 1) and (not fields[1].isdigit()):
                 # the second collumn in word entries contain the word literal
                 dictionary.add(fields[1])
@@ -40,36 +47,48 @@ def makeDictionary(filePath):
 # [string, set] -> string
 # take tokenized sentence and dictionary,
 # return tokenized words in sentence split by newlines
-def maxMatch(sentence, dictionary):
+def maxmatch(sentence, dictionary):
 
-    # base case for recursion
-    if len(sentence) < 2:
-        return "" # empty list of type string
+    # if sentence is empty, return empty string (base case for recursion)
+    if len(sentence) == 0:
+        return ''
 
-    # reversed, so start at end of sentence and work back
-    for i in reversed(range(2, len(sentence) - 1)):
-        # first i chars in sentence
+    # reversed because want to start at end of sentence and work back
+    # range begins at 2 because str[0:0] is an empty string (useless to check)
+    #    and str[0:1] is always a string of one character (which we always assume
+    #    is a word so it's pointless search for it in the dictionary)
+    # range ends at len(sentence) + 1 because len(sentence) excludes last
+    #    character
+    for i in reversed(range(2, len(sentence) + 1)):
+        # first i characters in sentence
         firstword = sentence[0 : i]
-
-        # rest of chars in sentence
+        # remaining characters in sentence
         remainder = sentence[i : len(sentence)]
 
-        # TODO somehow make this not recursive
-        if (firstword in dictionary) or (firstword.isdigit()):
-            return firstword + '\n' + maxMatch(remainder, dictionary)
+        # if firstword is in dictionary, then it's a word (first condition)
+        # if firstword is a continuous string of numbers, then it's a word
+        #    (second condition); note that this doesn't work for phone numbers
+        #    and equations and stuff, need something more sophisticated for that
+        if (firstword in dictionary) or firstword.isdigit():
+            return firstword + '\n' + maxmatch(remainder, dictionary)
 
-        # also this one
-        if i == 2:
-            return firstword + maxMatch(remainder, dictionary)
+    firstword = sentence[0 : 1]
+    remainder = sentence[1 : len(sentence)]
+    return firstword + '\n' + maxmatch(remainder, dictionary)
+    
 
+if len(sys.argv) != 2 :
+    print('error: not enough args')
+    print('maxmatch.py needs a file make a dictionary from.')
+    print('usage:\n\tpython maxmatch.py path/to/file')
+else :
+    dictionary = makeDictionary(sys.argv[1])
 
-dictionary = makeDictionary(sys.argv[1])
+    sample = sys.stdin.read()
+    # assume input has been segmented
+    sentences = sample.split('\n')
 
-sample = sys.stdin.read()
-# assume input has been segmented
-sentences = sample.split('\n')
-
-for sentence in sentences:
-    tokens = maxMatch(sentence, dictionary)
-    sys.stdout.write(tokens + '\n')
+    for sentence in sentences:
+        tokens = maxmatch(sentence, dictionary)
+        sys.stdout.write(tokens)
     
